f14ea5bd8494946a931ac3a013da810ba2e71228
huyn80303-bit <huyn80303@gmail.com>
2026-05-12 23:59:13 +0700

    Update INutrition.java

--- app/src/main/java/com/group3/model/INutrition.java
+++ app/src/main/java/com/group3/model/INutrition.java
@@ -1,5 +1,9 @@
 package com.group3.model;
 
 public interface INutrition {
-
+String getFoodName();
+    double getCalories();
+    double getProtein();
+    double getCarbs();
+    double getFat();
 }
