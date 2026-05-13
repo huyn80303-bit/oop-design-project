aa293008084023b8458927e6f6a4291a6492b183
huyn80303-bit <huyn80303@gmail.com>
2026-05-13 16:17:39 +0700

    Implement nutrition lookup and logging methods
    Added methods to lookup and save nutrition data.

--- app/src/main/java/com/group3/controller/NutritionLogController.java
+++ app/src/main/java/com/group3/controller/NutritionLogController.java
@@ -1,5 +1,19 @@
 package com.group3.controller;
 
-public class NutritionLogController {
+import com.group3.model.Nutrition;
+import com.group3.model.OpenFoodFactsAPI;
 
+public class NutritionLogController {
+private final OpenFoodFactsAPI openFoodFactsAPI = new OpenFoodFactsAPI();
+
+   
+    public Nutrition lookupNutrition(String productName) throws Exception {
+        return openFoodFactsAPI.fetchNutritionData(productName);
+    }
+
+    public void saveNutritionLog(Nutrition nutrition) {
+    
+        System.out.println("Saved: " + nutrition.getFoodName()
+            + " | Cal: " + nutrition.getCalories());
+    }
 }
